5ffbc896a09fc25a2ddd2df4c2d29e143588c23a
Michael Hernandez <gammabeta010@gmail.com>
2020-10-16 20:55:13 +0200

    Testing in progress of Tree

---
+++ Tree.java
@@ -0,0 +1,15 @@
+package project_jekelius;
+
+public class Tree {
+
+	int value;
+	
+	Tree left, right;
+	
+	public Tree (int value, Tree left, Tree right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+}
--- jekelius.java
+++ jekelius.java
@@ -1,10 +1,24 @@
 package project_jekelius;
+import javax.swing.JOptionPane;
+
 
 public class jekelius {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String name, parentName;
+		
+		int test = 5;
+		
+		name = JOptionPane.showInputDialog("Please enter a name");
+		
+		parentName = JOptionPane.showInputDialog("Please enter a parent name");
+		
+		Tree testing = new Tree(test, null, null);
 
 	}
+	
+	
 
 }
